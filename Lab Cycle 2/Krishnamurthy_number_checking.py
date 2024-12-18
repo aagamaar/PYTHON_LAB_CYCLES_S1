03e2565c718b4c00210f13dc253705e6087819b9
aagamaar <aagamaar01@gmail.com>
2024-12-18 06:43:10 +0530

    Krishnamurthy number checking added

---
+++ Lab Cycle 2/Krishnamurthy_number_checking.py
@@ -0,0 +1,13 @@
+import math
+n=input("Enter the number: ")
+s=0
+for i in n:
+    s += math.factorial(int(i)) 
+if s==int(n):
+    print(n,"is a Krishnamurthy number.")
+else:
+    print(n,"is not a Krishnamurthy number.")
+
+
+
+
