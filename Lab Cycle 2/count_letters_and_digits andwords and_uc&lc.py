08273e0dd74b0c9a6e82b6652bbf1fe48a7cd400
aagamaar <aagamaar01@gmail.com>
2024-12-18 12:28:18 +0530

    Count letter uc, lc, digits

---
+++ Lab Cycle 2/count_letters_and_digits andwords and_uc&lc.py
@@ -0,0 +1,21 @@
+n=input("Enter string: ")
+m=n.strip()
+l=d=w=uc=lc=0
+for i in m:
+    if i.isalpha():
+        l+=1
+        if i.isupper():
+            uc+=1
+        else:
+            lc+=1
+    else:
+        if i.isdigit():
+            d+=1
+f=m.split()
+print("Letters:",l)
+print("Digits:",d)
+print("Uppercase:",uc)
+print("Lowercase:",lc)
+print("Words:",len(f))
+
+
